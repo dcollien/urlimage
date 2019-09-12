52bbd4f7fa3f742bd5a5234db8fe25c1d8f5d4ee
David Collien <david@openlearning.com>
2019-09-12 16:03:40 +1000

    update urlimage package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="urlimage-dcollien",
+    name="urlimage",
     version="0.0.1",
     author="David Collien",
     author_email="me@dcollien.com",
